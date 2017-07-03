1572f3449f8e8d1d326adc0542b0878deadd1b35
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-07-03 22:37:21 +0300

    feat(resnet): Downloading weights

---
+++ resnet/download_weights.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# By using curl
+curl -O https://www.dropbox.com/s/txb16f1khleyvdh/ResNet-L50.npy?dl=1
+curl -O https://www.dropbox.com/s/8w10cs6v4rd9616/ResNet-L101.npy?dl=1
+curl -O https://www.dropbox.com/s/n5inup5a7fi8lom/ResNet-L152.npy?dl=1
+
+# or by using wget
+# wget https://www.dropbox.com/s/txb16f1khleyvdh/ResNet-L50.npy?dl=1
+# wget https://www.dropbox.com/s/8w10cs6v4rd9616/ResNet-L101.npy?dl=1
+# wget https://www.dropbox.com/s/n5inup5a7fi8lom/ResNet-L152.npy?dl=1
