c2afbca0f82d52fb4aceee96f2aadc1be8abca7c
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-11-19 19:33:53 +0300

    alexnet: Finetune for directions

--- alexnet/examples.sh
+++ alexnet/examples.sh
@@ -1,10 +1,41 @@
 #!/bin/sh
 
 python finetune.py \
-	--learning_rate "0.00001" \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
+	--train_layers "fc8"
+
+python finetune.py \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
+	--train_layers "fc8,fc7"
+
+python finetune.py \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
 	--train_layers "fc8,fc7,fc6"
 
 python finetune.py \
-	--num_epochs 30 \
+	--learning_rate "0.0003" \
 	--multi_scale "228,256" \
-	--train_layers "fc8,fc7,fc6,conv5,conv4,conv3,conv2,conv1" # full training
+	--train_layers "fc8,fc7,fc6,conv5"
+
+python finetune.py \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
+	--train_layers "fc8,fc7,fc6,conv5,conv4"
+
+python finetune.py \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
+	--train_layers "fc8,fc7,fc6,conv5,conv4,conv3"
+
+python finetune.py \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
+	--train_layers "fc8,fc7,fc6,conv5,conv4,conv3,conv2"
+
+python finetune.py \
+	--learning_rate "0.0003" \
+	--multi_scale "228,256" \
+	--train_layers "fc8,fc7,fc6,conv5,conv4,conv3,conv2,conv1"
