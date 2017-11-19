b8e9a06f0e85f2ba7d2be21ebb65cef457625d42
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-11-19 12:13:59 +0300

    vggnet: Shuffle split and cross validation for training svm

--- vggnet/train_svm.py
+++ vggnet/train_svm.py
@@ -3,6 +3,8 @@ from sklearn.decomposition import PCA
 from sklearn import svm
 import datetime
 from sklearn.externals import joblib
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
 
 print("Reading all_deep_representations_pca_merged.npy")
 all_deep_representations = np.load("./all_deep_representations_pca_merged.npy")
@@ -14,12 +16,23 @@ y = np.concatenate((y_pos, y_neg))
 
 print("{} Fitting SVM...".format(datetime.datetime.now()))
 clf = svm.SVC()
-clf.fit(all_deep_representations, y)
 
-print("{} Fitted, calculating score...".format(datetime.datetime.now()))
-score = clf.score(all_deep_representations, y)
-print(score)
+# First
+# clf.fit(all_deep_representations, y)
 
-print("{} Saving...".format(datetime.datetime.now()))
-joblib.dump(clf, 'svm.pkl') 
+# print("{} Fitted, calculating score...".format(datetime.datetime.now()))
+# score = clf.score(all_deep_representations, y)
+# print(score)
+
+# print("{} Saving...".format(datetime.datetime.now()))
+# joblib.dump(clf, 'svm.pkl') 
 # clf = joblib.load('svm.pkl') 
+
+# Second
+# scores = cross_val_score(clf, all_deep_representations, y, cv=10)
+# print("Accuracy: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
+
+# Third
+cv = ShuffleSplit(n_splits=10, test_size=0.1, random_state=None)
+scores = cross_val_score(clf, all_deep_representations, y, cv=cv)
+print("Accuracy: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
