125e343be28ec095c6885989d5d9a2b0b2474df5
Deniz Gurkaynak <dgurkaynak@gmail.com>
2018-02-21 23:56:53 +0300

    fix(resnet): Fix downloading weights

--- resnet/download_weights.sh
+++ resnet/download_weights.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # By using curl
-curl -O https://www.dropbox.com/s/txb16f1khleyvdh/ResNet-L50.npy?dl=1
-curl -O https://www.dropbox.com/s/8w10cs6v4rd9616/ResNet-L101.npy?dl=1
-curl -O https://www.dropbox.com/s/n5inup5a7fi8lom/ResNet-L152.npy?dl=1
+curl -O https://deniz.co/tensorflow-cnn-finetune/ResNet-L50.npy
+curl -O https://deniz.co/tensorflow-cnn-finetune/ResNet-L101.npy
+curl -O https://deniz.co/tensorflow-cnn-finetune/ResNet-L152.npy
 
 # or by using wget
 # wget https://www.dropbox.com/s/txb16f1khleyvdh/ResNet-L50.npy?dl=1
