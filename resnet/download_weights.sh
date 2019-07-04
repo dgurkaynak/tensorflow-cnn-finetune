d8e2d1d525fade03b751ccc8af3aa81f8d4e8ba2
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-07-04 14:43:03 +0300

    fix(resnet): Update weight urls

--- resnet/download_weights.sh
+++ resnet/download_weights.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # By using curl
-curl -O https://deniz.co/tensorflow-cnn-finetune/ResNet-L50.npy
-curl -O https://deniz.co/tensorflow-cnn-finetune/ResNet-L101.npy
-curl -O https://deniz.co/tensorflow-cnn-finetune/ResNet-L152.npy
+curl -O https://deniz.co/static/tensorflow-cnn-finetune/ResNet-L50.npy
+curl -O https://deniz.co/static/tensorflow-cnn-finetune/ResNet-L101.npy
+curl -O https://deniz.co/static/tensorflow-cnn-finetune/ResNet-L152.npy
 
 # or by using wget
 # wget https://www.dropbox.com/s/txb16f1khleyvdh/ResNet-L50.npy?dl=1
